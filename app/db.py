9ea8e73641b71cfac4c54d47b152ae48d7dfca64
ammar-coh <ammarkhn91@gmail.com>
2025-08-12 12:01:02 +0500

    .env.example

--- app/db.py
+++ app/db.py
@@ -1,11 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
 
-DB_USER = "ammar"
-DB_PASSWORD = "1234"
-DB_HOST = "localhost"
-DB_PORT = "5432"
-DB_NAME = "taxaidb"
+# Load variables from .env
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD =  os.getenv("DB_PASSWORD")
+DB_HOST =  os.getenv("DB_HOST")
+DB_PORT =  os.getenv("DB_PORT")
+DB_NAME =  os.getenv("DB_NAME")
 
 DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
